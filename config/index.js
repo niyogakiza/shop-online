fb8a5e0fc8a37b3ad42e11ff4fb03e664706319f
Aimable <niyoai@yahoo.fr>
2018-04-08 20:25:43 +0100

    Some scss and image upload

---
+++ config/index.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const OpenBrowser = require('open-browser-webpack-plugin');
+
+const base = require('./packer/base');
+const port = 8080;
+const env = process.env.NODE_ENV;
+const config = env === 'development' ?
+    require('./packer/dev') :
+    require('./packer/prod');
+
+for(let key in config.entry){
+    config.entry[key].unshift(
+        `webpack-dev-server/client?http://localhost:${port}`,
+        'webpack/hot/dev-server'
+    );
+}
+
+config.plugins.push(new OpenBrowser({
+    url: `http://localhost:${port}`
+}));
+
+
+const compiler = webpack(config);
+
+new WebpackDevServer(compiler, {
+    contentBase: process.cwd(),
+    hot: true,
+    publicPath: base.publicPath,
+    stats: {
+        color: true
+    },
+    overlay:true,
+    historyApiFallback: true,
+    headers:{
+        "X-Custom-Header": "yes",
+        "X-Powered-By": "Fq"
+    }
+})
+.listen(port, (err) => {
+    if(err){
+        console.log(err.stack);
+    }
+    console.log(`Server is running at PORT: ${port}`);
+});
---
+++ config/packer/prod.js
@@ -0,0 +1,92 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackNotifier = require('webpack-notifier');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const base = require('./base');
+
+module.exports = {
+    context: base.rootPath,
+    entry: {
+        main: ['./src/index.js'],
+        common:['react', 'react-dom']
+    },
+    output: {
+        path: base.staticPath,
+        fileName: 'assets/[name]_[hash:5].js',
+        publicPath: base.publicPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                include: base.srcPath,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: {loader:'css-loader', options: {minimize: false, modules: false, localIdentName:'[name]_[local]_[hash:base64:5]'}}
+                })
+            },
+            {
+                test: /\.css$/,
+                include: base.libPath,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: {loader:'css-loader', options: {modules: false}}
+                })
+            },
+            {
+                test:/\.jsx?$/,
+                include: base.srcPath,
+                use:'babel-loader'
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot|svg|swf)$/,
+                use: {loader: 'file-loader', options:{name: 'assets/[name]_[sha512:hash:base64:7].[ext]'}}
+            },
+            {
+                test:/\.scss$/,
+                include: base.srcPath,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use:['css-loader', 'sass-loader']
+                })
+            }
+
+        ]
+    },
+    resolve: {
+        alias: {},
+        modules: [base.libPath, base.srcPath]
+    },
+    plugins:[
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV':JSON.stringify('production')
+            }
+        }),
+        new UglifyJSPlugin(),
+        new HtmlWebpackPlugin({
+            template:'./src/index.html',
+            filename:'index.html',
+            title: 'Test App',
+        }),
+        new ExtractTextPlugin({
+            filename:'assets/[name]_[hash].css',
+            allChunks: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            minChunks: false
+        }),
+        new webpack.ProvidePlugin({
+            React: 'react',
+            ReactDom: 'react-dom',
+            PT: 'prop-types'
+        }),
+        new WebpackNotifier({
+            title:'Compilation completed',
+            alwaysNotify: true,
+            contentImage: base.masterPath
+        })
+    ]
+};
